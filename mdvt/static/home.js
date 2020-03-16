1f88bf687be3f3165eee86932d979b721ee79c12
Gabriel Lee <chihonglee777@gmail.com>
2020-03-16 23:04:19 +0000

    Added form to select a specific media source

---
+++ mdvt/static/home.js
@@ -0,0 +1,7 @@
+$('#category-input').on('input', function() {
+    $('#option-category').prop('checked', true);
+});
+
+$('#tag-input').on('input', function() {
+    $('#option-tag').prop('checked', true);
+});
