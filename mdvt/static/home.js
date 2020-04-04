2ad20eeab8559c32736b9cd8565fa0c861b3e402
Gabriel Lee <chihonglee777@gmail.com>
2020-04-04 16:11:59 +0800

    Apply last user filter preference

--- mdvt/static/home.js
+++ mdvt/static/home.js
@@ -14,3 +14,7 @@ $('#query-settings-form').submit(function() {
     $('#tag-input').val($('#tag-input').val().replace(' ', '_'));
     return true;
 });
+
+$('#depict-start-btn').click(function() {
+    $('#query-settings-form').submit();
+})
