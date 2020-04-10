4222784004aebe2eb0e452199db80091c02e7038
Gabriel Lee <chihonglee777@gmail.com>
2020-04-10 12:59:27 +0800

    Added "triggered_change" field to Contribution model

--- mdvt/database/models.py
+++ mdvt/database/models.py
@@ -26,14 +26,15 @@ class UserSetting(db.Model):
 class Contribution(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    media_id = db.Column(db.Integer, nullable=False)
+    page_id = db.Column(db.Integer, nullable=False)
     data_type = db.Column(db.String(20), nullable=False)
     data = db.Column(db.String(20), nullable=False)
     undo = db.Column(db.Boolean, nullable=False, default=False)
+    triggered_change = db.Column(db.Boolean, nullable=False, default=False)
     time_created = db.Column(db.DateTime, nullable=False,
                              server_default=func.now())
 
     def __repr__(self):
         return '<Contribution {} {} {} {} {} {} {}>'.format(
-            self.id, self.user_id, self.media_id, self.data_type, self.data,
-            self.undo, self.time_created)
+            self.id, self.user_id, self.page_id, self.data_type, self.data,
+            self.undo, self.triggered_change, self.time_created)
